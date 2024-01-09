aef39937f9e106580187862d3b828f9cce17dc9e
scarlettbishop <dhimarvishwa@gmail.com>
2024-01-09 17:57:56 +0530

    first commit

--- setup.py
+++ setup.py
@@ -17,9 +17,10 @@ setup(
     description="A small package for Book Recommender System",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{scarlettbishop}/{Books-Recommender-System-Using-Machine-Learning}",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     author_email="dhimarvishwa@gmail.com",
     packages=[SRC_REPO],
+    license="MIT",
     python_requires=">=3.7",
     install_requires=LIST_OF_REQUIREMENTS
 )
